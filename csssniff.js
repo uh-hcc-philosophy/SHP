1a75590a04c9c928d03578791fd965d6497d1d52
rcpino <pine@hawaii.edu>
2026-07-03 11:15:33 -1000

    Add files via upload

---
+++ csssniff.js
@@ -0,0 +1,19 @@
+<!--
+  if (navigator.platform == "MacPPC") {
+    if (navigator.appName == "Netscape"){
+      if(parseFloat(window.navigator.appVersion) >= 5.0){
+        //document.write("<link rel='stylesheet' href='/css/sft1_mac.css' type='text/css'>")
+	document.write("<link rel='stylesheet' href='/css/sft1.css' type='text/css'>")
+      }
+      else{
+        document.write("<link rel='stylesheet' href='/css/sft1_mac.css' type='text/css'>")
+      }
+    }
+    else {
+      document.write("<link rel='stylesheet' href='/css/sft1.css' type='text/css'>")}
+    }
+  else {
+    document.write("<link rel='stylesheet' href='/css/sft1.css' type='text/css'>")
+  }
+// -->
+
